e75eb284cc7983bde4063df7a627209d2677f808
Mohamed Ebrahim <ebrahimm131@gmail.com>
2018-12-06 10:36:51 +0200

    Fixing some code 2

--- src/Singleton.java
+++ src/Singleton.java
@@ -1,7 +1,6 @@
 public class Singleton {
     private static  Singleton instance;
     private Singleton(){}
-
     public static Singleton getInstance(){
         if (instance == null){
             instance = new Singleton();
@@ -9,10 +8,7 @@ public class Singleton {
         }
         return instance;
     }
-
     public void Showmessage(){
         System.out.println("Created Object");
     }
-
-
 }
